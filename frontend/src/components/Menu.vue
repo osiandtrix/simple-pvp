9933cbc030c0b6db263a6366b3225d1573d36aa6
n3xistence <maxinies00@gmail.com>
2023-10-30 18:27:36 +0100

    Initial commit

---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Hello</h1>
+  <v-btn @click="increment">+</v-btn>
+  <v-btn @click="decrement">-</v-btn>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
